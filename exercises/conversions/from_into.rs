ce18516d22068c0832247b9574a69bb7c8ae2026
wanglufee <3812132676@qq.com>
2024-07-12 19:26:45 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,28 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s == "" {
+            return Person::default();
+        }
+        let v: Vec<_> = s.split(",").collect();
+        if v.len() != 2 || v[0] == "" || v[1] == ""{
+            return Person::default();
+        }
+        match v[1].parse::<usize>() {
+            Ok(age) => {
+                Person {
+                    name: String::from(v[0]),
+                    age,
+                }
+            },
+            _ => {
+                Person::default()
+            },
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +52,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s == "" {
+            return Err(ParsePersonError::Empty);
+        }
+        let v: Vec<_> = s.split(",").collect();
+        if v.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        if v[0] == "" {
+            return Err(ParsePersonError::NoName);
+        }
+        match v[1].parse::<usize>() {
+            Ok(age) => {
+                Ok(Person{
+                    name: String::from(v[0]),
+                    age,
+                })
+            },
+            Err(e) => {
+                Err(ParsePersonError::ParseInt(e))
+            }
+        }
     }
 }
 
